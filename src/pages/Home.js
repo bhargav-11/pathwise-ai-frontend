8c8082a30b4050680026d1d2dec98ebc06e1a7ca
kevalvithani <vithanikeval7298@gmail.com>
2023-09-04 17:00:40 +0530

    delete history issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Chat } from "../componets/Chat";
 import Sidebar from "../componets/Sidebar";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
 
 const Home = () => {
-  const apiurl = process.env.REACT_APP_API_URL; 
+  const apiurl = process.env.REACT_APP_API_URL;
   const [chathistoryList, setchathistoryList] = useState([]);
   const [chat_id, setchat_id] = useState(0);
   const [shouldReload, setShouldReload] = useState(false);
@@ -14,17 +14,17 @@ const Home = () => {
   const [selectedItemIndex, setSelectedItemIndex] = useState(null);
   const [isnewchat, setIsNewChat] = useState(false);
   const [historyFolderid, setHistoryFolderid] = useState("");
-  const [errormessage,setErrormessage] = useState("");
+  const [errormessage, setErrormessage] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
     const user_id = localStorage.getItem("user_id");
     axios
-      .get(apiurl + "/get_all_chat_history/" + user_id )
+      .get(apiurl + "/get_all_chat_history/" + user_id)
       .then((response) => {
-        if(response.status >= 200 && response.status <= 205){
+        if (response.status >= 200 && response.status <= 205) {
           setAllHistory(response.data);
-        } 
+        }
       })
       .catch((error) => {
         Swal.fire({
@@ -43,45 +43,63 @@ const Home = () => {
   }, [shouldReload]);
 
   const gethistory = (id, folderId) => {
-    axios.get(apiurl +"/get_chats/" + id).then((response) => {
-      if(response.status >= 200 && response.status <= 205){
-        setchathistoryList(response.data);
-        setchat_id(id);
-        setSelectedItemIndex(id);
-        setHistoryFolderid(folderId);
-        setIsNewChat(false);
-      }
-    }).catch((error)=>{
-      Swal.fire({
-        title: error.response.data.error,
-        icon: "error",
-        toast: true,
-        timer: 2000,
-        position: "top-right",
-        timerProgressBar: true,
-        showConfirmButton: false,
-      });
-    });
-  };
-  const deleteHistory = (id) => {
-    try{
-    axios.delete(apiurl +"/delete_chat_entry/"+id).then((response) => {
-      if(response.status >= 200 && response.status <= 205){
-        setShouldReload((pre) => !pre);
-        setchathistoryList([]);
-        setIsNewChat(false);
-        setHistoryFolderid("");
+    axios
+      .get(apiurl + "/get_chats/" + id)
+      .then((response) => {
+        if (response.status >= 200 && response.status <= 205) {
+          setchathistoryList(response.data);
+          setchat_id(id);
+          setSelectedItemIndex(id);
+          setHistoryFolderid(folderId);
+          setIsNewChat(false);
+        }
+      })
+      .catch((error) => {
         Swal.fire({
-          title: response.data.message,
-          icon: "success",
+          title: error.response.data.error,
+          icon: "error",
           toast: true,
           timer: 2000,
           position: "top-right",
           timerProgressBar: true,
           showConfirmButton: false,
         });
-      }  
-    }).catch((error)=>{
+      });
+  };
+  const deleteHistory = (id) => {
+    try {
+      axios
+        .delete(apiurl + "/delete_chat_history/" + id)
+        .then((response) => {
+          if (response.status >= 200 && response.status <= 205) {
+            setShouldReload((pre) => !pre);
+            setchathistoryList([]);
+            setIsNewChat(false);
+            setHistoryFolderid("");
+
+            Swal.fire({
+              title: response.data.message,
+              icon: "success",
+              toast: true,
+              timer: 2000,
+              position: "top-right",
+              timerProgressBar: true,
+              showConfirmButton: false,
+            });
+          }
+        })
+        .catch((error) => {
+          Swal.fire({
+            title: error.response.data.error,
+            icon: "error",
+            toast: true,
+            timer: 2000,
+            position: "top-right",
+            timerProgressBar: true,
+            showConfirmButton: false,
+          });
+        });
+    } catch (error) {
       Swal.fire({
         title: error.response.data.error,
         icon: "error",
@@ -91,22 +109,11 @@ const Home = () => {
         timerProgressBar: true,
         showConfirmButton: false,
       });
-    });
-  }catch(error){
-    Swal.fire({
-      title: error.response.data.error,
-      icon: "error",
-      toast: true,
-      timer: 2000,
-      position: "top-right",
-      timerProgressBar: true,
-      showConfirmButton: false,
-    });
-  }
+    }
   };
 
   const clearhistory = () => {
-    setIsNewChat(!isnewchat)
+    setIsNewChat(!isnewchat);
     setchathistoryList([]);
     setchat_id(0);
     setSelectedItemIndex(0);
