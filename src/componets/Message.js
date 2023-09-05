b4e9e25a03dd6c872c9802f201f3ee85ccfeba36
kevalvithani <vithanikeval7298@gmail.com>
2023-09-05 19:48:50 +0530

    response structure change

--- src/componets/Message.js
+++ src/componets/Message.js
@@ -5,7 +5,7 @@ import { RiFileCopyLine } from "react-icons/ri";
 import Swal from "sweetalert2";
 
 const Message = ({ ask, response }) => {
-  const copyToclipboard = () =>{
+  const copyToclipboard = () => {
     navigator.clipboard.writeText(JSON.stringify(response));
     Swal.fire({
       title: "Response copied to clipboard",
@@ -16,19 +16,27 @@ const Message = ({ ask, response }) => {
       timerProgressBar: true,
       showConfirmButton: false,
     });
-  }
+  };
   let messageWithBreaks;
   if (response) {
-     messageWithBreaks = response.split('\n\n').map((paragraph, index) => (
+    messageWithBreaks = response.split("\n\n").map((paragraph, index) => (
       <React.Fragment key={index}>
-        {paragraph}
-        <br />
-        <br />
+        {paragraph.split("\n").map((line, lineIndex) => (
+          <React.Fragment key={lineIndex}>
+            {lineIndex > 0 && <br />} {line}
+          </React.Fragment>
+        ))}
+        {index < response.split("\n\n").length - 1 && (
+          <>
+            <br />
+            <br />
+          </>
+        )}
       </React.Fragment>
     ));
   } else {
     messageWithBreaks = (
-      <div className="error-message" style={{border:"2px solid"}}>
+      <div className="error-message" style={{ border: "2px solid" }}>
         Oops! Something went wrong or no response is available.
       </div>
     );
@@ -37,27 +45,23 @@ const Message = ({ ask, response }) => {
     <>
       <div className="message">
         <div className="message-info">
-        <div className="user mt-3">
+          <div className="user mt-3">
             <img src={user} />
-            <div className="message-content">
-             {ask}
-            </div>
+            <div className="message-content">{ask}</div>
           </div>
           {response?.length > 0 && (
-          <div className="other">
-            <img src={avtar} />
-            <div className="message-content">
-             {messageWithBreaks}
-              <div className="copy float-end mt-4">
-                <RiFileCopyLine onClick={copyToclipboard}/>
+            <div className="other">
+              <img src={avtar} />
+              <div className="message-content">
+                {messageWithBreaks}
+                <div className="copy float-end mt-4">
+                  <RiFileCopyLine onClick={copyToclipboard} />
+                </div>
               </div>
             </div>
-          </div>
-          )}         
+          )}
         </div>
-        
       </div>
-      
     </>
   );
 };
